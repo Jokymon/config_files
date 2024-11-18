32db08212e40d604592c6a85084d737bb4afff37
Silvan Wegmann <silvan.wegmann@hslu.ch>
2024-11-18 13:05:23 +0100

    Add a profile script for powershell

---
+++ ms_terminal/Profile.ps1
@@ -0,0 +1,6 @@
+Function SWEN { Set-Location -Path C:\projects\Module\SWEN\ }
+Function WEBT { Set-Location -Path C:\projects\Module\WEBT\ }
+Function TIP { Set-Location -Path C:\projects\Module\TIP\ }
+
+Function DOPY { .venv\Scripts\activate.ps1 }
+Function EDIT_PROFILE { gvim $HOME\Documents\PowerShell\Profile.ps1 }
